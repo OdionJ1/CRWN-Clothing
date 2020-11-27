ac858efe94f9fd2b78ee05bc424dc52a45c1fe78
OdionJ1 <58039048+OdionJ1@users.noreply.github.com>
2020-11-28 00:53:22 +0100

    added Link to h1 title on collection preview page

--- src/components/CollectionPreview.js
+++ src/components/CollectionPreview.js
@@ -2,10 +2,15 @@ import React from 'react'
 import './css/collectionPreview.scss'
 import CollectionItem from './CollectionItem'
 
+import { Link } from 'react-router-dom'
+
 export default function CollectionPreview({title, items}) {
     return (
         <div className='collection-preview'>
-            <h1 className='preview-title'>{title.toUpperCase()}</h1>
+            <Link to={`/shop/${title.toLowerCase()}`} style={{color: 'black', textDecoration: 'none'}}>
+                <h1 className='preview-title'>{title.toUpperCase()}</h1>
+            </Link>
+            
             <div className='preview'>
                 {items.filter((item, index) => index < 4).map(item => <CollectionItem key={item.id} item={item}/>)}
             </div>
